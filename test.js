7ec73b64db408d136331c6c24f8544ffbc5206ad
Saahilsodhi <ssodhi18@my.bcit.ca>
2024-09-16 11:01:12 -0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,27 @@
+const cold = "hot" 
+
+console.log("On a " + cold + " day in early winter, the chill seemed to seep into every corner of the town, making even the simplest tasks feel more daunting. The sun, hidden behind a blanket of gray clouds, did little to warm the air, and the day stretched on with a stark, frosty bite. People moved quickly, their breath visible in the frigid air, eager to return to the comfort of their homes. Despite the " + cold + ", there was a certain quiet beauty to the day, a serene calm that only a winter's chill can bring.")
+
+const day = "evevning" 
+//fix spelling on certain words
+
+
+cosnst name = "Saahil" //this variable does not print properly
+console.log("hi my name is " + name + " and my hobbies are listeing to music watching and playing sports and hanging out with my friends") 
+
+let bbq = "steak";
+console.log("i am eating "+bbq)
+//outputs "i am eating steak"
+
+
+console.log("My weekly pastime activities include watching football every Sunday and enjoying a " + bbq + " dinner with my family. The combination of football and " + bbq + " makes Sundays a special family tradition. I look forward to the football games all week, but the highlight is always the " + bbq + " dinner with my family. We gather together, savoring delicious " + bbq + " dishes while cheering on our favorite teams. Every Sunday, the blend of football excitement and the warmth of a family " + bbq + " makes for a perfect end to the week. My family and the " + bbq + " are the central parts of my Sunday routine, creating cherished memories that we all look forward to.")
+
+/*
+Hi Saahil, I like the start of your code where you declare your name and information. There are some minor spelling mistakes that doesn't affect the code but it does affect the reading aspect of it. You can try to include the constant in the your main story and also try the different console log with the ${} as it would change and improve your coding. other than that, everything else seems good, keep up the good work 
+-Reagan
+
+Hi Saahil, I like hte formationg of the console logs, but i have few suggestions. When declaring variables try and be mindful of how they are working, or if they were already declared. there are a couple erroes that could show up from them. Other then that organising the code might help a bit, week it up.
+
+-Diego B
+
+*/
